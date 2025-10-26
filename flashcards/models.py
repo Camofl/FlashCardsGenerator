6782825faab63967c06cdf1e0cbfc5e688ac5606
alfredweickert <alfred.weickert@descript.de>
2025-10-26 23:26:15 +0100

    initial commit

---
+++ flashcards/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from django_currentuser.db.models import CurrentUserField
+
+
+class Flashcard(models.Model):
+    front = models.TextField()
+    back = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    created_by = CurrentUserField(editable=False)
+    hidden = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.front
