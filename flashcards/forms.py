91e76bec98a6a80a78ed428bdea8d5f3e2e2a8a0
alfredweickert <alfred.weickert@descript.de>
2025-12-16 03:17:48 +0100

    Show form errors to user

--- flashcards/forms.py
+++ flashcards/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class BulkFlashcardRowForm(forms.Form):
     front = forms.CharField(max_length=255)
-    back = forms.CharField(required=False, widget=forms.Textarea(attrs={"rows": 2}))
+    back = forms.CharField(widget=forms.Textarea(attrs={"rows": 10}))
     hidden = forms.BooleanField(required=False, initial=False)
