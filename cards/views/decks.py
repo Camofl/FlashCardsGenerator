c5c9df552dd5e067b5159e6c03f8f17dbe5ec7b4
Camofl <186594915+Camofl@users.noreply.github.com>
2026-03-19 17:03:16 +0100

    Implement basic decks

--- cards/views/decks.py
+++ cards/views/decks.py
@@ -1,9 +1,50 @@
-from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, \
+    DeleteView
 
-from ..models import Flashcard
+from ..forms import DeckForm
+from ..models import Deck
 
 
-class FlashcardListView(ListView):
-    model = Flashcard
-    #template_name = "flashcards/decks/deck_list.html"
-    context_object_name = "flashcards"
+class DeckListView(ListView):
+    model = Deck
+    template_name = "cards/decks/list.html"
+    context_object_name = "decks"
+
+
+class DeckCreateView(LoginRequiredMixin, CreateView):
+    model = Deck
+    template_name = "cards/decks/form.html"
+    form_class = DeckForm
+    success_url = reverse_lazy("deck-list")
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+
+class DeckDetailView(DetailView):
+    model = Deck
+    template_name = "cards/decks/detail.html"
+    context_object_name = "deck"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        deck = self.object
+
+        context["flashcards"] = deck.flashcards.all()
+        return context
+
+
+class DeckUpdateView(UpdateView):
+    model = Deck
+    template_name = "cards/decks/form.html"
+    form_class = DeckForm
+    success_url = reverse_lazy("deck-list")
+
+
+class DeckDeleteView(DeleteView):
+    model = Deck
+    template_name = "cards/decks/confirm_delete.html"
+    success_url = reverse_lazy("deck-list")
